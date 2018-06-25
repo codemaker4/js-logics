babde5d844151b350485fbaf98e29a4f4eb8854a
CodeMaker_4 <thijs.rutledge@coderclass.nl>
2018-06-25 11:35:43 +0200

    beta 1.2.4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var xScreenSize = innerWidth - 5; // canvas size
 var yScreenSize = innerHeight - 5;
-var version = 'Beta 1.2.3'
+var version = 'Beta 1.2.4'
 var gateSize = 100;
 var connectionsVisible = true;
 var connectionOpacity = 127;
